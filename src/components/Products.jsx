a82a41d0f822e16de84f237f7199dc6bda897726
harith01 <harithyusuph@gmail.com>
2024-12-21 09:46:12 +0100

    Adjusted styling for products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,7 +6,7 @@ import ProductCard from './ProductCard'
 const Products = ({ products }) => {
     console.log(products.products)
   return (
-    <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 px-4 py-4 gap-4 w-screen items-center justify-center'>
+    <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 px-10 py-4 gap-4 w-screen items-center justify-center'>
         {products?.products?.map(product => <ProductCard key={product.id} product={product} />)}
     </div>
   )
