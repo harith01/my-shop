a7b6fc98b19dd5f2dd3ef1a3e9452dc870f3a0c7
harith01 <harithyusuph@gmail.com>
2024-12-28 14:59:26 +0100

    Implemented the remove item from cart feature

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { FaDeleteLeft, FaTrash } from 'react-icons/fa6'
 import ProductQuantity from './ProductQuantity'
+import { removeItem } from '../redux/cartSlice'
+import { useDispatch } from 'react-redux'
 
 const CartItem = ({ item }) => {
+  const dispatch = useDispatch()
   return (
     <div className='w-full shadow-md mb-3 px-2 py-2'>
       <div className='flex mb-2 items-center justify-between'>
@@ -19,7 +22,7 @@ const CartItem = ({ item }) => {
         </div>
       </div>
       <div className='flex justify-between'>
-        <div className='flex gap-2 items-center px-3 rounded-md text-red-600 hover:bg-red-300'>
+        <div onClick={() => dispatch(removeItem(item))} className='flex gap-2 items-center px-3 rounded-md text-red-600 hover:bg-red-300'>
           <FaTrash />
           <button>Remove</button>
         </div>
--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -57,6 +57,7 @@ const cartSlice = createSlice({
             state.totalQuantity--
             state.totalPrice -= itemToDecrease.price
         },
+    
     }
 })
 
