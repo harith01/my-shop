a2b66ac8ee6cf6cd6f0bbbf09d5db1f620eb2156
harith01 <harithyusuph@gmail.com>
2025-01-07 10:50:50 +0100

    Used teh AddCartButton component

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,33 +1,28 @@
 import React from 'react'
-import { addItem } from '../redux/cartSlice'
-import { useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom'
+import AddToCartButton from './AddToCartButton'
 
-const ProductCard = ({product}) => {
-    const dispatch = useDispatch()
-  return (
-    <div className='shadow-md px-5 py-2 rounded-md group'>
-        <div>
-            <img src={product.thumbnail} alt={product.title} />
+
+const ProductCard = ({ product }) => {
+
+    return (
+
+        <div className='shadow-md px-5 py-2 rounded-md group'>
+            <Link to={`/product/${product.id}`}>
+                <div>
+                    <img src={product.thumbnail} alt={product.title} />
+                </div>
+                <div className='pt-1 text-gray-800'>
+                    <h3>{product.title.slice(0, 20)}</h3>
+                </div>
+                <div className='font-semibold'>
+                    <p>$ {product.price}</p>
+                </div>
+            </Link>
+            <AddToCartButton product={product} />
         </div>
-        <div className='pt-1 text-gray-800'>
-            <h3>{product.title.slice(0,20)}</h3>
-        </div>
-        <div className='font-semibold'>
-            <p>$ {product.price}</p>
-        </div>
-        <div 
-        className='md:bg-white 
-         md:text-white group-hover:text-white 
-         group-hover:py-2 w-full
-         md:group-hover:bg-red-600
-         bg-red-600 h-10
-         mt-2
-         flex items-center justify-center'
-        >
-            <button onClick={() => dispatch(addItem(product))} className='uppercase'>Add to Cart</button>
-        </div>
-    </div>
-  )
+
+    )
 }
 
 export default ProductCard
