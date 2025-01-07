f9e106c1c7a51ebe724027526aee3786483b83dc
harith01 <harithyusuph@gmail.com>
2025-01-07 09:59:44 +0100

    Created AddToCartButton

---
+++ src/components/AddToCartButton.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { addItem } from '../redux/cartSlice'
+
+const AddToCartButton = ({ product }) => {
+    const dispatch = useDispatch()
+    return (
+        <div
+            className='md:bg-white 
+             md:text-white group-hover:text-white 
+             group-hover:py-2 w-full
+             md:group-hover:bg-red-600
+             bg-red-600 h-10
+             mt-2
+             flex items-center justify-center'
+        >
+            <button onClick={() => dispatch(addItem(product))} className='uppercase'>Add to Cart</button>
+        </div>
+    )
+}
+
+export default AddToCartButton
