9a98d33763612a578cc749dae0b18e1e3515f7bf
harith01 <harithyusuph@gmail.com>
2025-01-07 11:38:16 +0100

    Created the Review component

---
+++ src/components/Review.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import StarRating from './StarRating';
+
+const Review = ({ review }) => {
+    return (
+        <div className="mt-5">
+            <div className='flex items-center gap-2'>
+                <h3>{review.reviewerName}</h3>
+                <p>on {new Date(review.date).toLocaleDateString()}</p>
+            </div>
+            <StarRating rating={review.rating} />
+            <p>{review.comment}</p>
+        </div>
+    );
+};
+
+export default Review;
