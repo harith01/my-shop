1640339edd5207be3b2fabd077e6b5c427f05f78
harith01 <harithyusuph@gmail.com>
2025-01-08 16:34:28 +0100

    Created userSlice with 2 reducers

---
+++ src/redux/userSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+    {
+        username: 'harith',
+        email: 'harithy@ymail.com',
+        password: 'harith',
+    },
+    {
+        username: 'jamila',
+        email: 'jamy@ymail.com',
+        password: 'jammy'
+    }
+]
+
+const userSlice = createSlice({
+    name: 'users',
+    initialState,
+    reducers: {
+        addUser: (state, action) => {
+            state.push(action.payload)
+        },
+        removeUser: (state, action) => {
+            return state.filter(user => user.email !== action.payload)
+        }
+    }
+})
+
+export default userSlice.reducer
+export const { addUser, removeUser } = userSlice.actions
