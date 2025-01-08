67dab6040c1e18b2f1f50b25af9081fb9c132fc6
harith01 <harithyusuph@gmail.com>
2025-01-08 16:50:35 +0100

    Added authReducer and userReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,16 @@
 import productReducer from './productsSlice'
 import cartReducer from './cartSlice'
 import { configureStore } from '@reduxjs/toolkit'
+import authReducer from './authSlice'
+import userReducer from './userSlice'
 
 
 const store = configureStore({
     reducer: {
         products: productReducer,
         cart: cartReducer,
+        auth: authReducer,
+        users: userReducer
     }
 })
 
